99cd1f0ce964f04b4b57a59900c779945af0924c
Naufal Fauzan <naufalmnf1692006@gmail.com>
2025-05-25 12:44:35 +0700

    test

--- App.java
+++ App.java
@@ -6,6 +6,10 @@ public class App {
        stack.push(3);
        stack.push(4);
        stack.push(5);
+       stack.push(6);
+       stack.push(7);
+       stack.push(8);
+       stack.push(9);
 
        System.out.println("Stack output :");
        while (!stack.isEmpty()){
@@ -18,6 +22,11 @@ public class App {
         queue.insert(3);
         queue.insert(4);
         queue.insert(5);
+        queue.insert(6);
+        queue.insert(7);
+        queue.insert(8);
+        queue.insert(9); 
+        queue.insert(10);
 
        System.out.println("\nQueue output :");
        while (!queue.isEmpty()){
